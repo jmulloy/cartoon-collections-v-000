3a4fdf6307e9ab7d57820c198bdd16efbc9ea1ae
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-18 21:42:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+   dwarves.each.with_index(1) do |dwarf, index|
+     puts "#{index}. #{dwarf}."
+   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(different)
+  array.map {|different|
+  "#{different.capitalize}!"
+}
+
+
   # Your code here
 end
 
