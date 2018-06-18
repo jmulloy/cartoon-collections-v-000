aaa807f63fa34007d8bc9d939be183309e9db139
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-18 22:19:07 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(cheddar_cheese)
-  cheddar_cheese.include? "cheddar"
+  cheddar_cheese.include? ("cheddar")
 
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
