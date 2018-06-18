b45aa2c1a0e94e1d7a6c4a1f37a9c8615431d175
Josh Mulloy <joshmulloy1@gmail.com>
2018-06-18 22:58:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(calls)
-   calls.any? { |element| calls.length > 4 }
+   calls.any? { |call| call.length > 4 }
   # Your code here
 end
 
